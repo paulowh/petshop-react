45ddf49a981f7c2365ee5f85f8c75dc3c3d5e56d
Paulo-Not <paulo.whsantos@hotmail.com>
2023-09-18 15:38:27 -0300

    consumindo API com o axios

--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ListPost from "../components/ListPost";
 
 const Home = () => {
     return (
@@ -6,15 +7,7 @@ const Home = () => {
             <div className="container">
                 <h2 className="titulo-pagina">Sobre Meu amigo cão</h2>
             </div>
-            <section className="container flex flex--centro">
-                <article className="cartao">
-                    <h2 className="cartao__titulo">Banho no cão</h2>
-                    <p className="cartao__texto">Banhos regulares mantém os pelos e pele de seu cachorro
-                        limpa, renovada e saudável. O processo de ensaboar e escovar remove
-                        resíduos de pelos e peles que já estão mortas, providenciando um maior
-                        respiro da pele de seu cachorro</p>
-                </article>
-            </section>
+           <ListPost url={'/posts/'} />
         </main>
     )
 }
