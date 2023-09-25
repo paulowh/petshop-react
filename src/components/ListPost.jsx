4b9db5ac323ed74ad2c1268b589572bd6e79fa51
Paulo-Not <paulo.whsantos@hotmail.com>
2023-09-25 08:52:57 -0300

    Posts

--- src/components/ListPost.jsx
+++ src/components/ListPost.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { busca } from '../api/api'
 import { Link } from 'react-router-dom'
+
 const ListPost = ({ url }) => {
     const [posts, setPosts] = useState([])
     useEffect(() => {
